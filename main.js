c5df2363c3b28268de9a692e18bef297367a26dd
ossena <alessandro.ossena@myenaip.eu>
2025-10-16 09:33:31 +0000

    16/10/2025

---
+++ main.js
@@ -0,0 +1,42 @@
+//let array1 = ["🍔" , "old wild west" , "bacon" , "cheddar",];
+//console.log(array1);
+
+
+let oggetto1 = {
+    tipoPiatto: "panino",
+    catedoria: "fast food",
+    costo: 8,
+    successo: true,
+    rating: 4
+};
+
+//console.log(oggetto1);
+
+
+
+let azienda1 = {
+    Nome:"Pinco palla srl",
+    Luogo:"Lombardia",
+    Settore:"Software House",
+    NumeroSedi:"50",
+    Dipendenti:"100",
+    Telefono:" 📱 +39 ........ ",
+    Vendita:"computer 💻",
+};
+
+
+console.log(azienda1.Nome);
+console.log(azienda1.Luogo);
+console.log(azienda1.Settore);
+console.log(azienda1.NumeroSedi);
+console.log(azienda1.Telefono);
+console.log(azienda1.Vendita);
+console.log(azienda1.Dipendenti);
+
+
+let giochi1 = {
+    Multiplayer: "Minecraft",
+    singolPlayer: "DarkSouls",
+};
+
+//console.log(giochi1);
